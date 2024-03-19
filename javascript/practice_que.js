16d42b561f44b742e99dad7c0342b3b596aeb1a1
Priyanka Sahoo <97865839+Priyanka76910@users.noreply.github.com>
2024-03-19 18:38:37 +0000

    Added few que

---
+++ javascript/practice_que.js
@@ -0,0 +1,11 @@
+// Que-1 Print all even numbers from 0 to 100
+for(let i =0; i<=10; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
+
+// Que-2 game starts with any random numbers. Ask the  user to keep guessing the game number until the user enters correct value
+let gameNum = 25;
+let input =  prompt("guess the num");
+console.log(input);
