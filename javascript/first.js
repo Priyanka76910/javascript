f7696d6aaa02f29e6e28648302f9b28e9d01a65b
Priyanka Sahoo <97865839+Priyanka76910@users.noreply.github.com>
2024-03-19 18:23:36 +0000

    Key concept regarding loop

--- javascript/first.js
+++ javascript/first.js
@@ -1,1 +1,45 @@
-console.log("Priyanka");
+// Print 1 to 5 (FOR Loop)
+
+// let print = "Priyanka";
+// for(let i=0; i<5; i++){
+//     console.log(print);
+// }
+
+// WHILE Loop
+// let i=1;
+// while (i<5) {
+// console.log("i" , i);
+// i++;
+// }
+
+// Sum of 5 digit
+
+// let sum =0;
+// for(let i=1; i<=6; i++){
+    
+//     sum = sum+i;
+// }
+// console.log(sum);
+
+
+// FOR-of-LOOP -- Use for Array & String..
+
+//Length n Size
+let size = 0;
+
+let str = "PriyankA";
+for(let i of str){
+    console.log(i);
+    size++;
+}
+console.log(size);
+
+// FOR-in-Loop  -- Use for Objects/ Arrays
+let student = {
+    name : "Priyanka",
+    age : 20,
+    isPass : true
+};
+for(let key in student){
+   console.log(student[key]); 
+}
