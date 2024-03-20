402a35425580532899df2ef963cfe2bc74f4bb83
Priyanka Sahoo <97865839+Priyanka76910@users.noreply.github.com>
2024-03-20 18:46:46 +0000

    Nums and Math

---
+++ javascript/numsNmaths.js
@@ -0,0 +1,33 @@
+let score = 400
+// console.log(score);
+
+let balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(4));
+
+// console.log(123.908.toPrecision(3));
+ let hundred = 10000000
+//  console.log(hundred.toLocaleString());
+
+
+ // +++++++++++++++++++ Maths ++++++++++++++++++
+//  console.log(Math);
+//  console.log(Math.abs(-8));  // Only neg value to pos
+//  console.log(Math.round(4.6));
+//  console.log(Math.ceil(4.2));
+//  console.log(Math.floor(4.9));
+//  console.log(Math.min(2,3,4,5));
+//  console.log(Math.max(2,3,4,5,6));
+
+// console.log(Math.random()); // Value always range between o-1 
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);  
+
+let min = 10
+let max = 20
+
+console.log((Math.random() * (max - min + 1)) + min );
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
