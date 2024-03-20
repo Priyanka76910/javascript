457db77ff19b28440cebd8f55f5ebd240f5daac6
Priyanka Sahoo <97865839+Priyanka76910@users.noreply.github.com>
2024-03-20 18:16:00 +0000

    String

---
+++ javascript/string.js
@@ -0,0 +1,25 @@
+const name = "Priyanka"
+const repoCount = 90
+
+// console.log(name + repoCount + "Value");  //Outdated
+
+// Template Literals 
+
+// String Interpolation -  A way to have embedded expressions in Strings
+let sentence = `This is a template literal ${1+3}`;
+let say = `hello my name is ${name} and my repo count is ${repoCount}`;
+//  console.log(sentence);
+//  console.log(say);
+
+ const gameName = new String ('Priyanka-sahoo')
+
+//  console.log(gameName.substring(1,4));  // Here last character will not include eans here we write it will include upto 3
+
+//  console.log(gameName.slice(2,3)); // here we can give negative value
+
+ let newStr = "       heheh    "
+//  console.log(newStr);
+//  console.log(newStr.trim()); // remove extra space
+//  console.log(newStr.replace('h', 'po'));
+//  console.log(newStr.includes("p"));
+ console.log(gameName.split("-"));
