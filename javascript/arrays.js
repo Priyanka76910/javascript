a2c0f8a2cddad6690a2c749ff0feb0fad3e13d99
Priyanka Sahoo <97865839+Priyanka76910@users.noreply.github.com>
2024-03-22 17:55:15 +0000

    array continue

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -19,10 +19,34 @@ let newArr = myArr.join() // It will convert to String..
 
 //Slice , splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
 let new1 = myArr.slice(1,4)
-console.log(new1);
+// console.log(new1);
 
 let new2 = myArr.splice(1,2)
-console.log(new2);
+// console.log(new2);
+
+
+let mi = ["Rohit", "Pandya", "Ishan"]
+let rcb = ["Kohli", "Faf"]
+
+// mi.push(rcb);
+// console.log(mi);
+
+// console.log(mi.concat(rcb));
+
+let all = [...mi, ...rcb];
+// console.log(all);  // Spread
+
+let num = [1,2,3,[4,5],7,[6,7,[8,9,0]]]
+// console.log(num.flat(Infinity));
+
+console.log(Array.isArray("Priyaka"));
+console.log(Array.from("psahoo"));
+console.log(Array.from({name : "ps"}));  //Intresting..
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
